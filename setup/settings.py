fa39266dad1dcdfd5eca17b31d312c45e34e877d
roberto-cassio <rcslino99@gmail.com>
2024-11-06 22:09:08 -0300

    "fixed settings.py and added gitignore for .env file"

--- setup/settings.py
+++ setup/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*8=38ds6=-=%_cv%+zv+!+72#c5%pa+9_8jh37(lrk#!#ab3yh'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
