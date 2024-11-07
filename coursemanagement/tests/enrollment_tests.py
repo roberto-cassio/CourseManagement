ff7ea4efce5c3d3cea35761ad2e353473871d49d
roberto-cassio <rcslino99@gmail.com>
2024-11-07 19:46:14 -0300

    fixed test cases

--- coursemanagement/tests/enrollment_tests.py
+++ coursemanagement/tests/enrollment_tests.py
@@ -3,6 +3,7 @@ from django.forms import ValidationError
 from coursemanagement.models.students_registration import StudentRegistration
 from coursemanagement.models.students import Student
 from coursemanagement.models.courses import Courses
+from coursemanagement.models.teachers import Teacher
 from coursemanagement.services.enrollment_service import enroll_student
 from django.utils import timezone
 '''
@@ -11,8 +12,9 @@ TestCase para verificar se Alunos com Matrícula ativa retornam Flag Correta
 
 @pytest.mark.django_db
 def test_enroll_student_successful_registration():
-    student = Student.objects.create(name="Estudante Teste")
-    course = Courses.objects.create(name="Curso Teste")
+    student = Student.objects.create(name="Estudante Teste", email="teste@exemplo.com")
+    teacher = Teacher.objects.create(name="Professor Teste", email="testeprofessor@exemplo.com")
+    course = Courses.objects.create(title="Curso Teste", workload=40, teacher=teacher)
     enrollment_date = timezone.now()
     cancellation_date = None
 
@@ -29,8 +31,9 @@ TestCase para verificar o retorno se Aluno já está matrícula no Curso
 '''
 @pytest.mark.django_db
 def test_enroll_student_already_enrolled():
-    student = Student.objects.create(name="Estudante Teste")
-    course = Courses.objects.create(name="Curso Teste")
+    student = Student.objects.create(name="Estudante Teste", email="teste@exemplo.com")
+    teacher = Teacher.objects.create(name="Professor Teste", email="testeprofessor@exemplo.com")
+    course = Courses.objects.create(title="Curso Teste", workload=40, teacher=teacher)
     enrollment_date = timezone.now()
     cancellation_date = None
 
@@ -45,8 +48,9 @@ TestCase para verificar se Alunos com Matrícula Cancelada retornam Flag Correta
 
 @pytest.mark.django_db
 def test_enroll_student_with_past_cancellation_date():
-    student = Student.objects.create(name="Estudante Teste")
-    course = Courses.objects.create(name="Curso Teste")
+    student = Student.objects.create(name="Estudante Teste", email="teste@exemplo.com")
+    teacher = Teacher.objects.create(name="Professor Teste", email="testeprofessor@exemplo.com")
+    course = Courses.objects.create(title="Curso Teste", workload=40, teacher=teacher)
     enrollment_date = timezone.now()
     cancellation_date = timezone.now() - timezone.timedelta(days=1)  # Data passada
 
@@ -60,8 +64,9 @@ TestCase para verificar se Alunos com Matrícula ativa retornam Flag Correta
 
 @pytest.mark.django_db
 def test_enroll_student_with_future_enrollment_date():
-    student = Student.objects.create(name="John Doe")
-    course = Courses.objects.create(name="Django 101")
+    student = Student.objects.create(name="Estudante Teste", email="teste@exemplo.com")
+    teacher = Teacher.objects.create(name="Professor Teste", email="testeprofessor@exemplo.com")
+    course = Courses.objects.create(title="Curso Teste", workload=40, teacher=teacher)
     enrollment_date = timezone.now() + timezone.timedelta(days=1)  # Data futura
     cancellation_date = None
 
