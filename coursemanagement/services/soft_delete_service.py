93112846e4e0d9ccc9aabd18643bc5f8dcd27e6d
roberto-cassio <rcslino99@gmail.com>
2024-11-10 19:03:38 -0300

    Added cascade delete between courses and classes

--- coursemanagement/services/soft_delete_service.py
+++ coursemanagement/services/soft_delete_service.py
@@ -5,6 +5,8 @@ from rest_framework.exceptions import APIException
 
 from coursemanagement.models.courses import Courses
 from coursemanagement.models.students_registration import StudentRegistration
+from coursemanagement.models.classes import  Classes
+
 
 
 class DeletionError(APIException):
@@ -20,8 +22,10 @@ def is_deleted(obj):
 
 '''
 Inicialmente eu estava dando um update com o deleted_at, mas em uma perspectiva de escalabilidade percebi que isso não era muito prático.
-Dessa forma, para garantir a consistência entre as deleções, posso chamar o método destroy do próprio Courses, o que acaba gerando o efeito cascata esperado
+Dessa forma, para garantir a consistência entre as deleções, posso chamar o método destroy do próprio Courses, o que acaba gerando o efeito cascata esperado com o soft_delete.
 '''
+
+
 def delete_teacher_and_associated_courses(teacher):
     try:
         with transaction.atomic():
@@ -43,6 +47,17 @@ def delete_course_and_associated_registrations(course):
         StudentRegistration.objects.filter(courses=course, is_active=True).update(
             is_active=False, cancellation_date=timezone.now()
         )
+        
+        delete_classes_from_course(course)
     except Exception as e:
         raise DeletionError (detail=f"Erro ao deletar o curso {course.id}: {e}")
 
+def delete_classes_from_course(course):
+    '''Deleção das aulas associadas ao curso que está sendo deletado'''
+    try:
+        classes = Classes.object.filter(courses=course, deleted_at__isnull=True)
+        for class_object in classes:
+            class_object.deleted_at = timezone.now()
+            class_object.save()
+    except Exception as e:
+        raise DeletionError(detail=f"Erro ao deletar as aulas do course {course.id}: {e}")
