99573908c547ea2cca0fc379fe25d224314a825d
roberto-cassio <rcslino99@gmail.com>
2024-11-11 20:58:02 -0300

    fix: Added validation to check if student and course aren't deleted before enrolling them

--- coursemanagement/services/enrollment_service.py
+++ coursemanagement/services/enrollment_service.py
@@ -2,13 +2,25 @@ from django.forms import ValidationError
 from django.utils import timezone
 
 from coursemanagement.models.students_registration import StudentRegistration
-
+from coursemanagement.models.students import Student
+from coursemanagement.models.courses import Courses
 
 
 def enroll_student(student, course):
     '''Aqui fica a lógica de Matrícula do Aluno em um Curso + Validações'''
+    #Adicionada validação para checar se o aluno e curso para qual estamos tentando criar a matrícula não estão deletado
+    student_exists = Student.objects.filter(id=student.id, deleted_at__isnull = True).exists()
+    course_exists = Courses.objects.filter(id=course.id, deleted_at__isnull = True).exists()
+
+    if not student_exists:
+        raise ValidationError("Aluno não encontrado.")
+    
+    if not course_exists:
+        raise ValidationError("Curso não encontrado.")
+
     if StudentRegistration.objects.filter(student=student, courses=course, is_active=True).exists():
         raise ValidationError("O Aluno já está Matriculado nesse Curso.")
+    
     is_active = True
     enrollment_date = timezone.now()
     registration = StudentRegistration(student=student, courses=course, enrollment_date = enrollment_date, cancellation_date = None, is_active = is_active)
